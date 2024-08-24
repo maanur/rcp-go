c0e41d06c9e3718672136f486f5aea181879cbc3
maanur <onmyouji777@gmail.com>
2024-08-24 15:39:33 +0300

    add: create floating IPs

--- rustack/floating.go
+++ rustack/floating.go
@@ -6,13 +6,16 @@ import (
 )
 
 type Floating struct {
+	manager   *Manager
 	ID        string `json:"id"`
+	VDC       *Vdc   `json:"vdc,omitempty"`
 	IpAddress string `json:"ip_address"`
 }
 
 func (m *Manager) GetFloating(id string) (fip *Floating, err error) {
 	path, _ := url.JoinPath("v1/floating", id)
 	err = m.Get(path, Defaults(), &fip)
+	fip.manager = m
 	return
 }
 
@@ -29,8 +32,37 @@ func (v *Vdc) GetFloatingByAddress(address string) (fip *Floating, err error) {
 	for i := 0; i < len(items); i++ {
 		if items[i].IpAddress == address {
 			fip = items[i]
+			fip.manager = v.manager
 			return
 		}
 	}
 	return nil, fmt.Errorf("ERROR. Address %s not found", address)
+}
+
+func (v *Vdc) CreateFloating() (*Floating, error) {
+	args := &struct {
+		VDC string `json:"vdc"`
+	}{
+		VDC: v.ID,
+	}
+	fip := new(Floating)
+	err := v.manager.Request("POST", "v1/port", args, fip)
+	if err != nil {
+		return nil, err
+	}
+	fip.manager = v.manager
+	return fip, nil
+}
+
+func (fip *Floating) Delete() error {
+	err := fip.manager.Delete(fmt.Sprintf("v1/port/%s", fip.ID), Defaults(), nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fip Floating) WaitLock() (err error) {
+	path := fmt.Sprintf("v1/port/%s", fip.ID)
+	return loopWaitLock(fip.manager, path)
 }
